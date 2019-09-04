2d79d53206b913d49d9fc522bd212ccb4cf029e7
Rehman Ali <rehmanali001@gmail.com>
2019-09-04 16:47:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,6 +46,9 @@ class Student < InteractiveRecord
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end 
- 
+  
+  def self.find_by_name
+    
+  end 
  
 end
