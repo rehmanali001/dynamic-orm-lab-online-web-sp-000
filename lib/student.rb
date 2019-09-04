a575bd37c6168e52a71a49300313ecbb2d1d18de
Rehman Ali <rehmanali001@gmail.com>
2019-09-04 16:48:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,6 +49,7 @@ class Student < InteractiveRecord
   
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
     
   end 
  
