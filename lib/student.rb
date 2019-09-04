7849e1f7e84bad6d6020eed98eab072019223ae0
Rehman Ali <rehmanali001@gmail.com>
2019-09-04 16:44:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student < InteractiveRecord
   def values_for_insert
     values = []
     self.class.column_names.each do |col|
-      values << "'#{send(col)}'" unless send(col).nil?
+      values << "#{send(col)}" unless send(col).nil?
   end 
   values.join(", ")
   end
